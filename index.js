4083472bc9b003597a93a5d955c0aba8280e9905
John Grant <johnkgrant@gmail.com>
2017-05-21 21:24:07 -0400

    Add more recent golf course listing

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ var ballparks = readBallparks();
 var majors = filterMajors(ballparks);
 var tripleA = filterTripleA(ballparks);
 var courses = readGolfCourses();
+var golfDigest = readGolfDigest();
 
 console.log("===========")
 console.log("Total Majors: " + majors.length);
 console.log("Total TripleA: " + tripleA.length);
 console.log("Total courses: " + courses.length);
+console.log("Golf Digest Top 100: " + golfDigest.length);
 
-tripleA = _.map(tripleA, function(park) {
-  var updated = Object.assign(park);
-  _.set(park, 'properties.marker-color', '#9F0000')
-  return updated
-})
-
-writeResult(majors, tripleA, courses)
+var results = _.flatten([majors, tripleA, courses, golfDigest])
+writeResult(results)
 
 console.log('done!')
 
@@ -33,9 +30,17 @@ function filterMajors(ballparks) {
 }
 
 function filterTripleA(ballparks) {
-  return _.filter(ballparks, function(ballpark) {
+  var parks =  _.filter(ballparks, function(ballpark) {
     return ballpark.properties.Class === 'Triple-A'
   })
+
+  parks = _.map(parks, function(park) {
+    var updated = Object.assign(park);
+    _.set(park, 'properties.marker-color', '#9F0000')
+    return updated
+  })
+
+  return parks
 }
 
 function readGolfCourses() {
@@ -65,13 +70,57 @@ function courseToGEOJson (course) {
   }
 }
 
-function writeResult(ballparks, tripleA, courses) {
-  var all = ballparks.concat(courses).concat(tripleA);
+function readGolfDigest() {
+  var courses = JSON.parse(fs.readFileSync('./golf_digest.json', 'utf8'))
 
+  return _.map(courses, function(course) {
+    var coords = _.map(course.google_coords, parseFloat)
+    return {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: _.take(coords, 2),
+      },
+      properties: {
+        Class: "GolfDigest",
+        Rank: course.post_name.split('-')[0],
+        Name: course.post_title,
+        descripton: course.pin_image.url,
+        Lat: coords[0],
+        Long: coords[1],
+        "marker-color": "#00FA00"
+      }
+    }
+  })
+
+  return courses
+}
+
+function golfDigestCourseToGEOJson (course) {
+  var coords = _.map(course.Point.coordinates.split(","), parseFloat)
+  return {
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: _.take(coords, 2),
+    },
+    properties: {
+      Class: "Golf",
+      Rank: course.name.split('.')[0],
+      Name: course.name,
+      descripton: course.description,
+      Lat: coords[1],
+      Long: coords[0],
+      "marker-color": "#007f00"
+    }
+  }
+}
+
+function writeResult(results) {
   var result = {
     name: "mashup",
     type: "FeatureCollection",
-    features: all,
+    features: results,
   }
 
   fs.writeFileSync('mashup.geojson', JSON.stringify(result, null, 2), 'utf8')
